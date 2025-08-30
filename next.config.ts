bc1683b379d2dcc0b13b473b6f6f8ffe7994adda
yuntaedu <dbsxoen0730@yonsei.ac.kr>
2025-08-30 16:45:47 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
-const repo = "my-academy";
+const repo = "emschool";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
